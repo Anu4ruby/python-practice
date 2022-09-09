bb2941fde5c7014a913b141e9f25dfbe7e4221da
anup1502 <anupama.iyengar@rackspace.com>
2022-09-09 15:22:05 -0400

    pyhton first class functions and oops to calculate speed distance and time

---
+++ oop_speed_distance_time_calc.py
@@ -0,0 +1,49 @@
+#lets calculate the speed distance and time using the object oriented python and first class functions
+#https://favtutor.com/blogs/python-switch-case
+class CalculatorSwitch:
+
+    def operation(self,selection):
+        operations = {'1': "speed", '2':"distance",'3':"time"}
+        try:
+          self.selection = operations[selection]
+          return getattr(self, self.selection)()
+        except KeyError:
+          print("invalid option")
+
+    def speed(self):
+        print("lets calculate speed")
+        try:
+            distance = input("enter the distance in miles ")
+            time = input("enter time taken in hours ")
+            print(int(distance) / int(time))
+        except ValueError:
+            print("invalid value")
+
+    def distance(self):
+        print("lets calculate distance")
+        try:
+            speed = input("enter the speed in miles/hr ")
+            time = input("enter time taken in hours ")
+            print(int(speed) * int(time))
+        except ValueError:
+            print("invalid value")
+
+
+
+    def time(self):
+        ("lets calculate time")
+        try:
+            distance = input("enter the distance in miles ")
+            speed = input("enter the speed in miles/hr ")
+            print(int(distance) / int(speed))
+        except ValueError:
+            print("invalid value")
+
+
+
+
+
+selection = input("choose from the following options: 1.speed, 2.distance 3.time ")
+
+calc = CalculatorSwitch()
+calc.operation(selection)
